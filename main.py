3cfa963542eb9f8c14785dbcff7160da72c1611c
TGM29 <theogmota@gmail.com>
2021-07-11 08:54:16 -0300

    Ajustando

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from pywhatkit import *
 def send_whats():
     phone = '+5511988110909'
     message = 'Bom dia pipis, dormiu bem?'
-    pywhatkit.sendwhatmsg_instantly(phone, message, 2, False)
+    pywhatkit.sendwhatmsg_instantly(phone, message,wait_time = 10 ,tab_close= True)
     print("Message sent")
 
 send_whats()
