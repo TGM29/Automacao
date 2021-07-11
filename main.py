7dae7355cf8854b3e723d72a6069079270b3529a
TGM29 <theogmota@gmail.com>
2021-07-11 10:10:59 -0300

    Deu errado

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from pywhatkit import *
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 def send_whats():
     phone = '+5511988110909'
@@ -6,4 +8,7 @@ def send_whats():
     pywhatkit.sendwhatmsg_instantly(phone, message,wait_time = 10 ,tab_close= True)
     print("Message sent")
 
-send_whats()
+
+
+
+get_wheather()
