d67b3c936713eecd4044ca840f769f5e79ab7a50
TGM29 <theogmota@gmail.com>
2021-07-11 13:21:05 -0300

    Tentando essa p

--- main.py
+++ main.py
@@ -8,7 +8,17 @@ def send_whats():
     pywhatkit.sendwhatmsg_instantly(phone, message,wait_time = 10 ,tab_close= True)
     print("Message sent")
 
-
+def get_wheather():
+    cidade = 'Campos do Jordão'
+    #Declare the browser
+    browser = webdriver.Chrome()
+    #Open the site
+    browser.get("https://www.climatempo.com.br")
+    browser.find_element_by_xpath('//*[@id="bt_modalSearch_mobile"]/i').click()
+    browser.find_element_by_xpath('//*[@id="searchGeneralMobile"]').click()
+    browser.find_element_by_xpath('//*[@id="searchGeneralMobile"]').send_keys(cidade)
+    temp_min = browser.find_element_by_xpath('//*[@id="current-weather-temperature"]').get_property('nodeValue')
+    print(temp_min)
 
 
 get_wheather()
