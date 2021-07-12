cf53df2b131833b350d53627b716b0be3f8b3aa6
TGM29 <theogmota@gmail.com>
2021-07-12 10:08:05 -0300

    Funcionando mas feio

--- main.py
+++ main.py
@@ -1,26 +1,29 @@
 from pywhatkit import *
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from time import sleep
 
-def send_whats():
-    phone = '+5511988110909'
-    message = 'Bom dia pipis, dormiu bem?'
-    pywhatkit.sendwhatmsg_instantly(phone, message,wait_time = 10 ,tab_close= True)
-    print("Message sent")
+
+
 
 def get_wheather():
     #Declare the browser
     browser = webdriver.Chrome()
     #Open Climatempo
     browser.get('https://www.climatempo.com.br/previsao-do-tempo/cidade/420/camposdojordao-sp')
-    temp_min = browser.find_element_by_xpath('/html/body/div[4]/div[5]/div[4]/div[1]/div[2]/div[2]/div[2]/div[1]/ul/li[1]/div/p/span[1]').text
-    print(temp_min)
 
 
+    temp_min = browser.find_element_by_xpath('//*[@id="min-temp-1"]').text
+    temp_max = browser.find_element_by_xpath('//*[@id="max-temp-1"]').text
+    chuva = browser.find_element_by_xpath('//*[@id="mainContent"]/div[5]/div[4]/div[1]/div[2]/div[2]/div[2]/div[1]/ul/li[2]/div/span').text
+    raios_uv = browser.find_element_by_xpath('//*[@id="mainContent"]/div[5]/div[4]/div[2]/div[2]/div[2]/ul/li[4]/div[2]/p[2]').text
+    sol = browser.find_element_by_xpath('//*[@id="mainContent"]/div[5]/div[4]/div[2]/div[2]/div[2]/ul/li[6]/div[2]/p[2]').text
 
-    #Open the site
-    dolar = browser.find_element_by_xpath('//*[@id="knowledge-currency__updatable-data-column"]/div[1]/div[2]/span[1]').get_attribute('data-value')
+    phone = '+5511988110909'
+    message = f'Bom dia pipis dormiu bem?\nA temperatura máxima de hoje será de {temp_max} e mínima de {temp_min}.\nHoje a chance de chuva é de {chuva}.\nA incidencia de raios UV hoje é {raios_uv}, portanto {sol}.\nBom dia e te amo muito'
 
+    pywhatkit.sendwhatmsg_instantly(phone, message, wait_time=10, tab_close=True)
+    print("Message sent")
 
 
 get_wheather()
